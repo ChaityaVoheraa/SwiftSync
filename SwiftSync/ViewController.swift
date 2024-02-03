d301255a4e8a6aa0bf8f077789478365c1a62da0
Chaitya Vohera <vohera.c@northeastern.edu>
2024-02-03 13:45:50 -0500

    Initial Commit

---
+++ SwiftSync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftSync
+//
+//  Created by Chaitya Vohera on 03/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
